a2d7247c7b7ca7a4f1f742e7fe54aeee8e70717f
Rujaun Fourie <rujaun@gmail.com>
2020-10-29 14:04:05 +0200

    Added vim-plug

--- install.sh
+++ install.sh
@@ -40,6 +40,16 @@ if [ "$GIT" = "Y" ]; then
 	git config --global pull.rebase false
 fi
 
+echo -e "\nInstall Vim-plug? (Y/N): "
+
+read VIMPLUG
+
+if [ "$VIMPLUG" = "Y" ]; then
+	echo -e "\nInstalling vim-plug..."
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 
 echo -e "\nInstall Windows Fonts? (Y/N): "
 
