ada7ab36e0ccbbeeb84759dcdba8a6c7b3b69346
Rujaun Fourie <rujaun@gmail.com>
2020-11-28 14:47:18 +0200

    Added alacritty config and couple of tweaks

---
+++ fonts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Font config
+sudo cp ./.local.conf /etc/fonts/.local.conf
---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Git
+git config --global user.name "Rujaun Fourie"
+git config --global user.email rujaun@gmail.com
+git config --global pull.rebase false
